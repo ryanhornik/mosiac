472098c65fc5f477c06974b8ba9c7eb79095cd4e
Ryan Hornik <ryan.hornik@towbee.com>
2016-04-18 18:00:39 -0500

    Added ability for user to select sub-image size from a list of factors

--- mosaic.py
+++ mosaic.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+from functools import reduce
 
 from PIL import Image
 
@@ -49,26 +50,48 @@ def get_mosaic_array(image, width, height):
     return averaged_images
 
 
-def stitch_image_from_array(arr, size):
-    stitched = Image.new(mode="RGB", size=size)
-    width, height = stitched.size
+def stitch_image_from_array(arr, full_size, sub_image_size):
+    stitched = Image.new(mode="RGB", size=full_size)
 
-    col = 0
-    row = 0
-    for j in range(0, height, 144):
-        for i in range(0, width, 256):
+    full_width, full_height = stitched.size
+    sub_image_width, sub_image_height = sub_image_size
+
+    column_count = full_width // sub_image_width
+    row_count = full_height // sub_image_height
+
+    row, col = 0, 0
+    for j in range(0, full_height, sub_image_height):
+        for i in range(0, full_width, sub_image_width):
             stitched.paste(arr[row][col], (i, j))
-            col = (col + 1) % 10
-        row = (row + 1) % 10
+            col = (col + 1) % column_count
+        row = (row + 1) % row_count
 
     return stitched
 
 
-def main(filename, output, width=256, height=144):
+def factors(n):
+    return set(reduce(list.__add__, ([i, n // i] for i in range(1, int(n ** 0.5) + 1) if n % i == 0)))
+
+
+def get_user_factor_selection(n, name):
+    n_factors = sorted(factors(n))
+    i = 1
+    for i, value in enumerate(n_factors):
+        print("({0: >2}) {1: <12}".format(i, value), end="")
+        if (i+1) % 5 == 0:
+            print()
+    return n_factors[int(input("{}Select a factor for sub-image {}\n".format("" if (i+1) % 5 == 0 else "\n", name)))]
+
+
+def main(filename, output):
     image = Image.open(filename)
+    img_width, img_height = image.size
+
+    width = get_user_factor_selection(img_width, "width")
+    height = get_user_factor_selection(img_height, "height")
 
     averaged_images = get_mosaic_array(image, width, height)
-    stitch_image_from_array(averaged_images, size=image.size).save(output)
+    stitch_image_from_array(averaged_images, full_size=image.size, sub_image_size=(width, height)).save(output)
 
 
 if __name__ == "__main__":
